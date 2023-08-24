e163fed26def2ef62493b909d2038266906011fb
gustavolara <gustavolara63719@gmail.com>
2023-08-24 14:09:54 -0300

    finalizado o movieDesc

--- src/components/movieDescription/movieDescription.jsx
+++ src/components/movieDescription/movieDescription.jsx
@@ -11,7 +11,7 @@ const MovieDesc = ({ movies, click }) => {
         fetch(apiUrl)
             .then((response) => response.json())
             .then((data) => setMovieDesc(data))
-            .catch((error) => console.log("Erro: ", error))
+            .catch((error) => console.error("Erro: ", error))
     }, [apiUrl]);
 
     const movie = movieDesc;
@@ -20,7 +20,7 @@ const MovieDesc = ({ movies, click }) => {
         <div className={styles.modalBackdrop} onClick={click}>
             <div className={styles.movieModal} onClick={(e) => e.stopPropagation()}>
                 <div className={styles.movieInfo}>
-                    <img src={movie.Poster} alt={movie.Title} />
+                    <img src={movies.Poster} alt={movie.Title} />
                     <button className={styles.btn} onClick={click}>X</button>
                     <div className={styles.movieType}>
                         <div>
